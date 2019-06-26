6a42a207c5158aba1c5d090c66664d587ac33b2c
Georgia <georgiapsn@hotmail.com>
2019-06-26 08:23:59 +0100

    Add GameController shared instance and automatic increment method

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController SharedInstance;
+    Biodome biodome;
+    public Text money;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        biodome = new Biodome();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncrementMoney()
+    {
+        biodome.money = biodome.money + biodome.ticketPrice;
+
+        money.text = "Money: " + biodome.money.ToString();
+    }
+}
